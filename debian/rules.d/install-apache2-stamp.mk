048b604b1b67ce9e2bfa5984ab3aed650c2c39cf
Ondřej Surý <ondrej@sury.org>
2015-10-17 14:25:18 +0200

    Remove debian/ to prepare for subtree add

--- debian/rules.d/install-apache2-stamp.mk
+++
@@ -1,4 +0,0 @@
-install-apache2-stamp: build-apache2-stamp
-	install -d -m 755 debian/tmp/etc/apache2/mods-available
-	dh_auto_install --builddirectory apache2-build -- install-headers install-build install-sapi install-programs INSTALL_ROOT=$(CURDIR)/debian/tmp
-	touch install-apache2-stamp
