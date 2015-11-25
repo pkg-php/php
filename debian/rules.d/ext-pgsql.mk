0f1d61d89853113f254129fa44874efcee25e91b
Ondřej Surý <ondrej@sury.org>
2015-11-25 11:25:02 +0100

    Add missing ./configure options for all modules

--- debian/rules.d/ext-pgsql.mk
+++ debian/rules.d/ext-pgsql.mk
@@ -1,5 +1,7 @@
 ext_PACKAGES += pgsql
 pgsql_DESCRIPTION := PostgreSQL
-pgsql_EXTENSIONS := pgsql pdo_pgsql
+pgsql_EXTENSIONS  := pgsql pdo_pgsql
+pgsql_config      := --with-pgsql=shared,/usr
+pdo_pgsq_config   := --with-pdo-pgsql=shared,/usr
 export pgsql_EXTENSIONS
 export pgsql_DESCRIPTION
