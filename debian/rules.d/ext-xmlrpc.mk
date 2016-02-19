6eb8cd7c9a4064a9202384e56f076b531944bddd
Ondřej Surý <ondrej@sury.org>
2016-02-19 12:56:03 +0100

    Use bundled libxmlrpc for PHP 5.5 xmlrpc extension

--- debian/rules.d/ext-xmlrpc.mk
+++ debian/rules.d/ext-xmlrpc.mk
@@ -1,6 +1,6 @@
 ext_PACKAGES     += xmlrpc
 xmlrpc_DESCRIPTION := XMLRPC-EPI
 xmlrpc_EXTENSIONS  := xmlrpc
-xmlrpc_config      := --with-xmlrpc=shared,/usr
+xmlrpc_config      := --with-xmlrpc=shared
 export xmlrpc_EXTENSIONS
 export xmlrpc_DESCRIPTION
