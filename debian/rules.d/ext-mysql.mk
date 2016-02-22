5c0afb2b6e85594e109365eb9f7fcf316da542e4
Ondřej Surý <ondrej@sury.org>
2016-02-22 11:49:29 +0100

    Move mysqlnd to phpX.Y-mysql extension package

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -1,7 +1,13 @@
 ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
 mysql_EXTENSIONS  := mysqli pdo_mysql
-mysqli_config     := --with-mysqli=shared,mysqlnd
+mysqli_config     := \
+	--enable-mysqlnd \
+	  --enable-mysqlnd-compression-support \
+	  --with-zlib-dir=/usr \
+	--with-mysqli=shared,mysqlnd
 pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
+mysqlnd_PRIORITY  := 10
+export mysqlnd_PRIORITY
 export mysql_EXTENSIONS
 export mysql_DESCRIPTION
