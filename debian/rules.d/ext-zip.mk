2c923297081154f0e46671fae489659531b4fd0d
Ondřej Surý <ondrej@sury.org>
2016-02-22 14:41:27 +0100

    Split bcmath, calendar, ctype, exif, fileinfo, ftp, gettext, iconv, pdo, phar, posix, shmop, sockets, sysv*, tokenizer extensions into phpX.Y-common package; add zip to extra phpX.Y-zip
    Conflicts:
    	debian/control.in

---
+++ debian/rules.d/ext-zip.mk
@@ -0,0 +1,10 @@
+ext_PACKAGES    += zip
+zip_DESCRIPTION := Zip
+zip_EXTENSIONS  := zip
+zip_config = \
+	--enable-zip=shared \
+	--with-zlib-dir=/usr \
+	--with-pcre-dir=/usr \
+	--with-libzip=/usr
+export zip_EXTENSIONS
+export zip_DESCRIPTION
