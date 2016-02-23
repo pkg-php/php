b42de5bcbc90c2caaa5e5f2948cb73423a1a1cf8
Ondřej Surý <ondrej@sury.org>
2016-02-23 11:54:56 +0100

    Add missing mysqlnd shared module back to phpX.Y-mysqlnd package

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -1,11 +1,8 @@
 ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
-mysql_EXTENSIONS  := mysqli pdo_mysql
-mysqli_config     := \
-	--enable-mysqlnd \
-	  --enable-mysqlnd-compression-support \
-	  --with-zlib-dir=/usr \
-	--with-mysqli=shared,mysqlnd
+mysql_EXTENSIONS  := mysqlnd mysqli pdo_mysql
+mysqlnd_config    := --enable-mysqlnd=shared --enable-mysqlnd-compression-support --with-zlib-dir=/usr
+mysqli_config     := --with-mysqli=shared,mysqlnd
 pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
 mysqlnd_PRIORITY  := 10
 export mysqlnd_PRIORITY
