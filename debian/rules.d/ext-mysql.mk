b1ce7e8fa98da1af945a1abbee53c5983322cf50
Ondřej Surý <ondrej@sury.org>
2016-04-26 10:46:39 +0200

    Enable DEPRECATED mysql driver

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -1,6 +1,7 @@
 ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
-mysql_EXTENSIONS  := mysqlnd mysqli pdo_mysql
+mysql_EXTENSIONS  := mysql mysqlnd mysqli pdo_mysql
+mysql_config      := --with-mysql=shared,mysqlnd
 mysqlnd_config    := --enable-mysqlnd=shared --enable-mysqlnd-compression-support --with-zlib-dir=/usr
 mysqli_config     := --with-mysqli=shared,mysqlnd
 pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
