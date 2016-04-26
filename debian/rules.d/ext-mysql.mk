c1e47c06e70e487d48ff500c6495390992fee947
Ondřej Surý <ondrej@sury.org>
2016-04-26 11:22:35 +0200

    Sync with PHP 5.6 packaging

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -1,7 +1,11 @@
 ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
-mysql_EXTENSIONS  := mysqli pdo_mysql
+mysql_EXTENSIONS  := mysql mysqlnd mysqli pdo_mysql
+mysql_config      := --with-mysql=shared,mysqlnd
+mysqlnd_config    := --enable-mysqlnd=shared --enable-mysqlnd-compression-support --with-zlib-dir=/usr
 mysqli_config     := --with-mysqli=shared,mysqlnd
 pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
+mysqlnd_PRIORITY  := 10
+export mysqlnd_PRIORITY
 export mysql_EXTENSIONS
 export mysql_DESCRIPTION
