1efca8b17377813df488bf176695f8b44c5215b3
Ondřej Surý <ondrej@sury.org>
2017-03-01 14:31:16 +0100

    Enable mssql extension in php5.6-sybase package

--- debian/rules.d/ext-sybase.mk
+++ debian/rules.d/ext-sybase.mk
@@ -1,6 +1,7 @@
 ext_PACKAGES       += sybase
 sybase_DESCRIPTION := Sybase
-sybase_EXTENSIONS  := pdo_dblib
+sybase_EXTENSIONS  := pdo_dblib mssql
 pdo_dblib_config   := --with-pdo-dblib=shared,/usr
+mssql_config       := --with-mssql=shared,/usr
 export sybase_EXTENSIONS
 export sybase_DESCRIPTION
