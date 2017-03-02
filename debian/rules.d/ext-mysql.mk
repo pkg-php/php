b986fbaf53606288f062d8c2066e30fe84e29ade
Ondřej Surý <ondrej@sury.org>
2017-03-02 11:27:31 +0100

    Sync debian packaging for PHP 5.6, 7.0 and 7.1

--- debian/rules.d/ext-mysql.mk
+++ debian/rules.d/ext-mysql.mk
@@ -1,11 +1,14 @@
 ext_PACKAGES      += mysql
 mysql_DESCRIPTION := MySQL
-mysql_EXTENSIONS  := mysql mysqlnd mysqli pdo_mysql
-mysql_config      := --with-mysql=shared,mysqlnd
+mysql_EXTENSIONS  := mysqlnd mysqli pdo_mysql
 mysqlnd_config    := --enable-mysqlnd=shared --enable-mysqlnd-compression-support --with-zlib-dir=/usr
 mysqli_config     := --with-mysqli=shared,mysqlnd
 pdo_mysql_config  := --with-pdo-mysql=shared,mysqlnd
 mysqlnd_PRIORITY  := 10
+ifeq ($(PHP_MAJOR_VERSION),5)
+mysql_EXTENSIONS  += mysql
+mysql_config      := --with-mysql=shared,mysqlnd
+endif
 export mysqlnd_PRIORITY
 export mysql_EXTENSIONS
 export mysql_DESCRIPTION
--- debian/rules.d/ext-sybase.mk
+++ debian/rules.d/ext-sybase.mk
@@ -1,7 +1,10 @@
 ext_PACKAGES       += sybase
 sybase_DESCRIPTION := Sybase
-sybase_EXTENSIONS  := pdo_dblib mssql
+sybase_EXTENSIONS  := pdo_dblib
 pdo_dblib_config   := --with-pdo-dblib=shared,/usr
+ifeq ($(PHP_MAJOR_VERSION),5)
+sybase_EXTENSIONS  += mssql
 mssql_config       := --with-mssql=shared,/usr
+endif
 export sybase_EXTENSIONS
 export sybase_DESCRIPTION
